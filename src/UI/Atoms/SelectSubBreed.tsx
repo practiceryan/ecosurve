299c1a1275dbc6b4f456fa930b96fc08c83d7344
Ryan Hague <iamryan41@gmail.com>
2022-05-31 21:23:31 +0100

    Atoms

---
+++ src/UI/Atoms/SelectSubBreed.tsx
@@ -0,0 +1,27 @@
+﻿import DogApi from "../../Data/Api/DogApi";
+import {Loader, Select} from "@mantine/core";
+
+const SelectSubBreed = (breed: string) => {
+    const {SubBreeds} = DogApi();
+    const query = SubBreeds(breed);
+
+    if (query.isLoading) {
+        return <Loader />
+    }
+
+    return (
+        <Select
+            label={"Breed"}
+            placeholder={"Select"}
+            data={query.data!.map((breed) =>
+            {
+                return {
+                    value: breed.name,
+                    label: breed.name
+                }
+            })}
+        />
+    )
+}
+
+export default SelectSubBreed;
