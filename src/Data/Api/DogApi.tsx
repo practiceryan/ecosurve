7153343160a1ad79cd5deb457819bc0fcc09a714
Ryan Hague <iamryan41@gmail.com>
2022-06-05 14:44:30 +0100

    api minor update

--- src/Data/Api/DogApi.tsx
+++ src/Data/Api/DogApi.tsx
@@ -125,7 +125,8 @@ const DogApi = () => {
                     }
                     return breed;
                 })
-            }
+            },
+            enabled: breed.length > 0
         }
     )
     
